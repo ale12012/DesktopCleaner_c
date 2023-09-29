28dfe3aa75cfe51a96ec5bfad0653a3f300802a9
AndKlet <andreaskletvang@outlook.com>
2023-09-29 07:11:39 +0200

    Fix minor bugs

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "src/cleanerfuncs.h"
+
+
+int main(int argc, char* argv[])
+{
+
+    char** files = getFiles();
+    char* s = malloc(10 * sizeof(char));
+    strcpy(s, getStatus());
+    char* desktopPath = malloc(255 * sizeof(char));
+    if (strcmp(s, "0") == 0)
+    {
+        printf("Setting hidden attribute\n");
+        logFiles();
+        for (int i = 0; i < 100; i++)
+        {
+            if (files[i] == NULL)
+            {
+                break;
+            }
+            else
+            {
+                strcpy(desktopPath, getDesktopPath());
+                strcat(desktopPath, "\\");
+                strcat(desktopPath, files[i]);
+                for (int j = 0; j < strlen(desktopPath); j++)
+                {
+                    if (desktopPath[j] == '\n')
+                    {
+                        desktopPath[j] = '\0';
+                    }
+                }
+                setAttribHidden(desktopPath);
+            }
+        }
+        changeStatus();
+    }
+    else
+    {
+        printf("Removing hidden attribute\n");
+        logFiles();
+        for (int i = 0; i < 100; i++)
+        {
+            if (files[i] == NULL)
+            {
+                break;
+            }
+            else
+            {
+                strcpy(desktopPath, getDesktopPath());
+                strcat(desktopPath, "\\");
+                strcat(desktopPath, files[i]);
+                for (int j = 0; j < strlen(desktopPath); j++)
+                {
+                    if (desktopPath[j] == '\n')
+                    {
+                        desktopPath[j] = '\0';
+                    }
+                }
+
+                if (strcmp(&desktopPath[0], ".") == 0 || strcmp(&desktopPath[0], "~") == 0 || strcmp(&desktopPath[0], "$") == 0 
+                || strstr(desktopPath, ".lnk") != NULL || strstr(desktopPath, ".ini") != NULL)
+                {
+                    continue;
+                }
+                removeAttribHidden(desktopPath);
+            }
+        }
+        changeStatus();
+    }
+
+}
